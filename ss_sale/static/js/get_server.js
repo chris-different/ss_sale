bbe33f162bcf4bbc923aa3d538dda3888a3a8bb7
chris-different <532604040@qq.com>
2018-03-18 20:43:19 +0800

    备份

--- ss_sale/static/js/get_server.js
+++ ss_sale/static/js/get_server.js
@@ -1,7 +1,26 @@
 window.onload=function(){
-	alert(1);
+	
+
 	$.get("/api/server",function(data,status){
-		for i in data:
-			console.log(i);
+		
+		$.each(data,function(i,n){
+
+
+
+
+			
+
+			$("div.container-fluid").append("<div class=\"col-md-4 col-sm-4 serverlist\" id=\""+i+"\"><\/div>");
+			var h = "#"+i;
+			$(h).append("<div class=\"serverlist-in\" id=\""+i+"\"><\/div>");	
+			$("div#"+i+" .serverlist-in").append("<ul id=\""+i+"\" class=\"ul-list\"><\/ul>");
+			$("#"+i+" ul").append("<li class=\"server\">Ip-address:"+n.ip_address+"<\/li>");
+			$("#"+i+" ul").append("<li class=\"server\">port:"+n.port+"<\/li>");
+			$("#"+i+" ul").append("<li class=\"server\">City-address:"+n.city_address+"<\/li>");
+			$("#"+i+" ul").append("<li class=\"server\">Timeout:"+n.timeout+"<\/li>");	
+				
+				
+				
+		});
 	});
 }
